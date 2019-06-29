75ac17580ab96ddabb5c73e6f22521a9a7e7458e
zhixian <zhixian@hotmail.com>
2019-06-29 19:29:17 +0800

    Update startup.cs for authentication

--- Inventory.WebApp/Startup.cs
+++ Inventory.WebApp/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,12 +32,13 @@ namespace Inventory.WebApp
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
 
-                // My custom consent cookie
+                // Overide consent cookie
                 options.ConsentCookie = new CookieBuilder()
                 {
                     Name = ".Inventory.Consent",            // Change from default of ".AspNet.Consent"
                     Expiration = TimeSpan.FromDays(365),    // 
                     IsEssential = true,                     // 
+                    //HttpOnly = true                       // Cookie won't be set if this true
                 };
 
                 options.Secure = CookieSecurePolicy.Always; // Default: CookieSecurePolicy.None
@@ -47,8 +50,49 @@ namespace Inventory.WebApp
                 
             });
 
+            // services.AddAuthorization(options => {
+            // });
+            // services.AddAuthorization(options =>
+            // {
+            //     options.AddPolicy("Over18", policy =>
+            //     {
+            //         policy.AuthenticationSchemes.Add(Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme);
+            //         policy.RequireAuthenticatedUser();
+            //         policy.Requirements.Add(new MinimumAgeRequirement());
+            //     });
+            // });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // Override antiforgery cookie
+            services.AddAntiforgery(options => {
+                options.Cookie.Name = "Inventory.AntiForgery";
+            });
+
+            // services.AddAuthentication(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme)
+            // .AddCookie(options => {
+            // });
+            services.AddAuthentication()
+            .AddCookie(options => {
+                options.LoginPath = "/Account/Unauthorized/";
+                options.AccessDeniedPath = "/Account/Forbidden/";
+            })
+            .AddJwtBearer(options => {
+                options.Audience = "http://localhost:5001/";
+                options.Authority = "http://localhost:5000/";
+            });
+
+            services.AddMvc(options => {
+                // Add filters here
+                //options.Filters.Add()
+                
+                // Add default policy which only allow authenticated users to view
+                // 
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+
+                options.Filters.Add(new AuthorizeFilter(policy));
+
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +101,7 @@ namespace Inventory.WebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -64,9 +109,24 @@ namespace Inventory.WebApp
                 app.UseHsts();
             }
 
+            // app.UseStatusCodePages(); // Status Code: 404; Not Found
+            // app.UseStatusCodePages("text/plain", "Status code page, status code: {0}");   
+            // app.UseStatusCodePages(async context =>
+            // {
+            //     context.HttpContext.Response.ContentType = "text/plain";
+
+            //     await context.HttpContext.Response.WriteAsync(
+            //         "Status code page, status code: " + 
+            //         context.HttpContext.Response.StatusCode);
+            // });
+            app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            // Use authentication
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
