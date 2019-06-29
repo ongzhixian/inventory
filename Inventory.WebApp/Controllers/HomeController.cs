24fc955ea701d409fb27fb64f0b7598282daab97
zhixian <zhixian@hotmail.com>
2019-06-29 19:29:55 +0800

    Securing HomeController

--- Inventory.WebApp/Controllers/HomeController.cs
+++ Inventory.WebApp/Controllers/HomeController.cs
@@ -5,16 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Inventory.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Inventory.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        //[AllowAnonymous]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -39,9 +42,40 @@ namespace Inventory.WebApp.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult SecurePage()
         {
+            //return StatusCode(403);
+            return View();
+        }
+
+        // [AllowAnonymous]
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // }
+
+        [AllowAnonymous]
+        public IActionResult Problem()
+        {
+
+            return StatusCode(403);
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int? id)
+        {
+            //HttpStatusCode.Forbidden
+
+            //System.Net.HttpStatusCode
+            if (id == 404 || id == 500)
+            {
+                var viewName = id.ToString();
+                return View(viewName);
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
