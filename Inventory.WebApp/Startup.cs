9fb38139f8619e76b21eff24f8f4d018ecb51f21
zhixian <zhixian@hotmail.com>
2019-07-06 08:04:40 +0800

    Add default routing for Area

--- Inventory.WebApp/Startup.cs
+++ Inventory.WebApp/Startup.cs
@@ -211,6 +211,12 @@ namespace Inventory.WebApp
 
             app.UseMvc(routes =>
             {
+                // Add default routes for area handling
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
