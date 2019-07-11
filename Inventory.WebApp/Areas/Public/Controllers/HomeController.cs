d313318a03ae06946da8acfda8c61daab0328086
zhixian <zhixian@hotmail.com>
2019-07-11 21:24:47 +0800

    Update Public

--- Inventory.WebApp/Areas/Public/Controllers/HomeController.cs
+++ Inventory.WebApp/Areas/Public/Controllers/HomeController.cs
@@ -5,13 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Csi.WebApp.Areas.Research
+namespace Csi.WebApp.Areas.Public
 {
     [AllowAnonymous]
     [Area("Public")]
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Salt()
         {
             return View();
         }
